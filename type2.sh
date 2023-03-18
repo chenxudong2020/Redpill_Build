7bd66a4b37101983126d863fb947fa13c3b66000
872603935@qq.com <872603935@qq.com>
2023-03-18 13:08:08 +0800

    fix

--- type2.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-[ -e config/DS920+/7.0.1-42218/config.json ] && sed -i -e 's;https://global.download.synology.com/download/DSM/release/7.0.1/42218/DSM_DS920%2B_42218.pat;https://transfer.sh/get/dVHsjX/DSM_DS920+_42218.pat;g' config/DS920+/7.0.1-42218/config.json
