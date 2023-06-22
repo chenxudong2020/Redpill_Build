0be30fa0c2b92557751ecab19eaa3e90dbf5e40b
chenxudong2020 <872603935@qq.com>
2023-06-22 02:30:54 +0000

    add

---
+++ version.sh
@@ -0,0 +1,5 @@
+ #!/usr/bin/env bash
+          sudo apt-get install -y jq
+          wget https://raw.githubusercontent.com/wjz304/redpill-load/main/config/configs.json
+          maxVersion=$(cat  ./configs.json | jq -r 'to_entries| .[]| select(.key == "DS920+")| .value.platforms' | jq -r 'keys[length-1]')
+          echo $maxVersion > version.txt
