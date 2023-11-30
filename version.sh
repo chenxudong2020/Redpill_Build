473b35b38fa734672cc3071dd980220f334b381b
chenxudong2020 <872603935@qq.com>
2023-11-30 09:33:51 +0800

    fix 423

--- version.sh
+++ version.sh
@@ -1,5 +1,6 @@
  #!/usr/bin/env bash
           sudo apt-get install -y jq
-          wget https://raw.githubusercontent.com/wjz304/redpill-load/main/config/configs.json
-          maxVersion=$(cat  ./configs.json | jq -r 'to_entries| .[]| select(.key == "DS920+")| .value.platforms' | jq -r 'keys[length-1]')
+          #wget https://raw.githubusercontent.com/wjz304/redpill-load/main/config/configs.json
+          #maxVersion=$(cat  ./configs.json | jq -r 'to_entries| .[]| select(.key == "DS920+")| .value.platforms' | jq -r 'keys[length-1]')
+          maxVersion= 7.2.1-69057
           sudo echo $maxVersion > ./version.txt
